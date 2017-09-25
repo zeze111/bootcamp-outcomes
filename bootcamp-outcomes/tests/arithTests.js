e4565d15046651dd64697d34e1c182ff35ea8104
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-09-25 16:39:58 +0100

    error corrections

--- bootcamp-outcomes/tests/arithTests.js
+++ bootcamp-outcomes/tests/arithTests.js
@@ -6,16 +6,6 @@ chai.should();
 const expect = chai.expect;
 
 describe('aritGeo', function () {
-  it('should check that array is not empty', function () {
-    const list = [];
-    app.aritGeo(list);
-    assert.equal(aritGeo(list), 0);
-  });
-  it('should check that parameter does not start with 0', function () {
-    const list = [0, 2, 4, 6];
-    aritGeo(list);
-    assert.equal(aritGeo(list), 0)
-  });
   it('should check that arithmetic is true', function () {
     const list = [2, 4, 6, 8, 10];
     aritGeo(list);
@@ -25,6 +15,16 @@ describe('aritGeo', function () {
     const list = [2, 4, 8, 16, 32];
     aritGeo(list);
     assert.equal(aritGeo(list), 'geometric');
+  });
+  it('should check that array is not empty', function () {
+    const list = [];
+    app.aritGeo(list);
+    assert.equal(aritGeo(list), 0);
+  });
+  it('should check that parameter does not start with 0', function () {
+    const list = [0, 2, 4, 6];
+    aritGeo(list);
+    assert.equal(aritGeo(list), 0)
   });
   it('should check that is not geometric/arithmetic', function () {
     const list = [1, 4, 7, 16, 32];
